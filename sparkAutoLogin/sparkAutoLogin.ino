c99dd115b2cb78a14a20dd94048c44f67e160ca3
ohmiclaser <44399381+ohmiclaser@users.noreply.github.com>
2019-06-15 16:59:49 +0100

    Added led blinking

--- sparkAutoLogin/sparkAutoLogin.ino
+++ sparkAutoLogin/sparkAutoLogin.ino
@@ -1,13 +1,29 @@
 #include <DigiKeyboard.h>
+int ledPin = 1;
 void setup() {
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin, HIGH); 
   DigiKeyboard.sendKeyStroke(0); 
   DigiKeyboard.print("user.name");// Please your username.
-  DigiKeyboard.delay(10);
-  DigiKeyboard.print("\t");//why the \t needs to have quotes I do not no but it works.
-  DigiKeyboard.delay(10);
+  DigiKeyboard.delay(10); 
+  DigiKeyboard.print("\t");//why the \t needs to have quotes I don't no but it works.
+  DigiKeyboard.delay(10); 
   DigiKeyboard.println("password");// please your password. 
+  digitalWrite(ledPin, LOW);
+  DigiKeyboard.delay(850);
+  DigiKeyboard.println();
+  digitalWrite(ledPin, HIGH);
+  delay(3000);
+  
 }
 void loop() {
-  
- 
+  delay(375);
+  digitalWrite(ledPin, LOW);
+  delay(375);
+  digitalWrite(ledPin, HIGH);
+
 }
+ 
+/*
+ 
+*/
